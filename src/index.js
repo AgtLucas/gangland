1b0327cc097c126f7326344c6fbc297424cef37f
AgtLucas <lucas@agtlucas.com>
2018-03-30 22:28:52 +0200

    Update src

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash'
+import './styles.css'
 
 function component () {
   var element = document.createElement('div')
 
   element.innerHTML = _.join(['Hello', 'Webpack'], ' ')
+  element.classList.add('el')
 
   return element
 }
